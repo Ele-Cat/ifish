4d0c87e9f491904cafa67a747b5f33613c63c7b5
lijiamao <951572198@qq.com>
2023-10-23 16:57:15 +0800

    移除无用图片,新增俩app的logo

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -6,6 +6,11 @@
       <img src="https://api.vvhan.com/api/moyu" alt="">
       <!-- https://www.sojson.com/other/relax.html -->
       <!-- <video src=""></video> -->
+
+      <!-- 60s读懂世界 -->
+      <!-- 图片 https://api.vvhan.com/api/60s -->
+      <!-- 简报 https://api.qqsuu.cn/api/dm-60s?type=json -->
+      <!-- 详细 https://www.zhihu.com/api/v4/columns/c_1261258401923026944/items -->
     </div>
   </div>
 </template>

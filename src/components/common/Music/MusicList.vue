9cc850322f8ccff99d74ade361526e255129ee61
lijiamao <951572198@qq.com>
2023-10-27 10:26:18 +0800

    加几个音乐源

--- src/components/common/Music/MusicList.vue
+++ src/components/common/Music/MusicList.vue
@@ -18,6 +18,11 @@ const emit = defineEmits(['close']);
 const close = () => {
   emit("close");
 }
+
+// 最新音乐排行榜top100 https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8¬ice=0&platform=h5&needNewCode=1&tpl=3&page=detail&type=top&topid=27&_=1519963122923
+// 随机推荐 https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8¬ice=0&platform=h5&needNewCode=1&tpl=3&page=detail&type=top&topid=36&_=1520777874472
+
+// 专辑封面 http://imgcache.qq.com/music/photo/album_300/17/300_albumpic_8217_0.jpg
 </script>
 
 <style lang="less" scoped>

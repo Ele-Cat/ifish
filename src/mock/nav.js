48fc1e58b1bb93673083d20b0015a9d8953b7db5
lijiamao <951572198@qq.com>
2023-11-09 17:17:50 +0800

    💄优化书签

--- src/mock/nav.js
+++ src/mock/nav.js
@@ -1560,28 +1560,13 @@ const nav = [
                 "name": "小程序开发",
                 "desc": "微信小程序",
                 "url": "https://developers.weixin.qq.com/miniprogram/dev/index.html",
-                "urls": {
-                  "英文": "https://developers.weixin.qq.com/miniprogram/en/dev/index.html?t=18082922",
-                  "中文": "https://developers.weixin.qq.com/miniprogram/dev/index.html"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "小游戏开发",
                 "desc": "微信小游戏",
-                "url": "https://developers.weixin.qq.com/minigame/dev/index.html?t=18082922",
-                "urls": {
-                  "英文": "https://developers.weixin.qq.com/minigame/en/dev/index.html?t=18081820",
-                  "中文": "https://developers.weixin.qq.com/minigame/dev/index.html?t=18082922"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://developers.weixin.qq.com/minigame/dev/guide/",
               }
             ],
-            "createdAt": "2021-01-23T01:32:06.423Z"
           },
           {
             "title": "小程序框架",
@@ -1590,143 +1575,68 @@ const nav = [
                 "name": "mpvue",
                 "desc": "Vue.js 开发小程序的前端框架。框架基于 Vue.js 核心",
                 "url": "http://mpvue.com/",
-                "urls": {
-                  "中文": "http://mpvue.com/",
-                  "Github": "https://github.com/Meituan-Dianping/mpvue"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "wepy",
                 "desc": "官方小程序组件化开发框架",
-                "url": "https://tencent.github.io/wepy/",
-                "urls": {
-                  "中文": "https://tencent.github.io/wepy/",
-                  "Github": "https://github.com/Tencent/wepy"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://wepyjs.gitee.io/wepy-docs/",
               },
               {
-                "icon": "https://avatars0.githubusercontent.com/u/27521938?s=200&v=4",
                 "name": "mpx",
                 "desc": "Mpx是一款致力于提高小程序开发体验的增强型小程序框架(滴滴出品，基于vue)",
-                "url": "https://didi.github.io/mpx/",
-                "urls": {
-                  "中文": "https://didi.github.io/mpx/",
-                  "Github": "https://github.com/didi/mpx"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://wepyjs.gitee.io/wepy-docs/",
               },
               {
-                "icon": "https://avatars1.githubusercontent.com/u/5485517?s=200&v=4",
                 "name": "uni-app",
                 "desc": "uni-app 是使用 Vue 语法开发小程序、H5、App的统一框架",
-                "url": "https://uniapp.dcloud.io/",
-                "urls": { "Github": "https://github.com/dcloudio/uni-app" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://uniapp.dcloud.net.cn/",
               },
-              {
-                "icon": "https://tencent.github.io/omi/assets/omi-logo2019.svg",
-                "name": "omix",
-                "desc": "腾讯原生小程序框架 OMIX 2.0",
-                "url": "https://github.com/Tencent/omi/tree/master/packages/omix",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              }
             ],
-            "createdAt": "2021-01-23T01:32:06.423Z"
           },
           {
             "title": "小程序组件库/插件",
             "nav": [
               {
-                "icon": "https://avatars2.githubusercontent.com/u/11404085?s=200&v=4",
                 "name": "vant-weapp",
                 "desc": "轻量、可靠的小程序 UI 组件库",
-                "url": "https://vant-contrib.gitee.io/vant-weapp/#/intro",
-                "urls": { "Github": "https://github.com/youzan/vant-weapp" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://vant-contrib.gitee.io/vant-weapp/#/home",
               },
               {
-                "icon": "https://avatars0.githubusercontent.com/u/18461506?s=200&v=4",
                 "name": "weui-wxss",
                 "desc": "这是一套基于样式库weui-wxss开发的小程序扩展组件库，同微信原生视觉体验一致的UI组件库",
                 "url": "https://github.com/Tencent/weui-wxss/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "wxParse",
                 "desc": "wxParse-微信小程序富文本解析自定义组件，支持HTML及markdown解析",
                 "url": "https://github.com/icindy/wxParse",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "mpvue-wxParse",
                 "desc": "适用于 Mpvue 的微信小程序富文本解析自定义组件",
                 "url": "https://github.com/F-loat/mpvue-wxParse",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://avatars1.githubusercontent.com/u/2268460?s=200&v=4",
                 "name": "echarts-for-weixin",
                 "desc": "ECharts 的微信小程序版本",
                 "url": "https://github.com/ecomfe/echarts-for-weixin",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://lbs.qq.com/favicon.ico",
                 "name": "腾讯位置服务",
                 "desc": "微信小程序原生LBS能力的最佳拍档",
-                "url": "https://lbs.qq.com/qqmap_wx_jssdk/index.html",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
+                "url": "https://lbs.qq.com/miniProgram/jsSdk/jsSdkGuide/jsSdkOverview",
               },
               {
                 "name": "ColorUI",
                 "desc": "鲜亮的高饱和色彩，专注视觉的小程序组件库",
-                "url": "https://www.color-ui.com/",
-                "urls": { "Github": "https://github.com/weilanwl/ColorUI" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://github.com/weilanwl/ColorUI",
               },
               {
                 "name": "weapp-input-frame",
                 "desc": "验证码、支付密码输入框组件",
                 "url": "https://github.com/xjh22222228/weapp-input-frame",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               }
             ],
-            "createdAt": "2021-01-23T01:32:06.423Z"
           },
           {
             "title": "微信公众号开发辅助工具",
@@ -1735,31 +1645,18 @@ const nav = [
                 "name": "JS接口签名校验工具",
                 "desc": "微信 JS 接口签名校验工具",
                 "url": "https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=jsapisign",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "接口调试工具",
                 "desc": "微信公众平台接口调试工具",
                 "url": "https://mp.weixin.qq.com/debug/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "computed",
                 "desc": "小程序自定义组件 computed / watch 扩展",
                 "url": "https://github.com/wechat-miniprogram/computed",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               }
             ],
-            "createdAt": "2021-01-23T01:32:06.423Z"
           },
           {
             "title": "资源",
@@ -1768,22 +1665,13 @@ const nav = [
                 "name": "awesome-github-wechat-weapp",
                 "desc": "微信小程序开源项目库汇总",
                 "url": "https://github.com/opendigg/awesome-github-wechat-weapp",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "awesome-wechat-weapp",
                 "desc": "微信小程序开发资源汇总",
                 "url": "https://github.com/justjavac/awesome-wechat-weapp",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               }
             ],
-            "createdAt": "2021-01-23T01:32:06.423Z"
           },
           {
             "title": "其他",
@@ -1792,33 +1680,18 @@ const nav = [
                 "name": "微信开放社区",
                 "desc": "小程序开发者专区，微信公众平台,微信开放社区,开发者专区",
                 "url": "https://developers.weixin.qq.com/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "wechat-format",
                 "desc": "微信公众号排版编辑器，转换 Markdown 到微信特制的 HTML",
-                "url": "https://lab.lyric.im/wxformat/",
-                "urls": {
-                  "Github": "https://github.com/lyricat/wechat-format"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
+                "url": "https://github.com/lyricat/wechat-format",
               },
               {
                 "name": "微信开放平台",
                 "desc": "微信开放平台",
                 "url": "https://open.weixin.qq.com/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               }
             ],
-            "createdAt": "2021-01-23T01:32:06.423Z"
           }
         ],
       },

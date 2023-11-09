cf756cfa12451f0e18ba5ac6500b605143dd6211
lijiamao <951572198@qq.com>
2023-11-09 09:50:24 +0800

    💄优化书签

--- src/mock/nav.js
+++ src/mock/nav.js
@@ -9,381 +9,72 @@ const nav = [
             "title": "开发神器",
             "nav": [
               {
-                "name": "Github-CLI",
-                "createdAt": "2021-02-18T13:06:06.671Z",
-                "rate": 5,
-                "desc": "GitHub的官方命令行工具",
-                "top": false,
-                "icon": "https://github.githubassets.com/favicons/favicon.svg",
-                "url": "https://cli.github.com/",
-                "urls": { "Github": "https://github.com/cli/cli" }
-              },
-              {
-                "name": "Coveralls",
-                "icon": "https://coveralls.io/favicon.png",
-                "url": "https://coveralls.io/",
-                "desc": "通过显示测试套件未涵盖代码的哪些部分，我们可以帮助您自信地交付代码。免费提供开源软件库",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "Releasly",
-                "desc": "在新的开源版本发布时收到通知，直接发送到您的电子邮件中。",
-                "url": "https://www.releasly.co/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "Convert cURL",
-                "desc": "将curl命令转换为Python，JavaScript，PHP，R，Go，Rust，Dart，JSON，Ansible，Elixir。",
-                "url": "https://curl.trillworks.com/",
-                "urls": {
-                  "Github": "https://github.com/NickCarneiro/curlconverter"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "Supervisor",
-                "desc": "Supervisor是一个客户端/服务器系统，允许其用户监视和控制类似UNIX的操作系统上的多个进程。",
-                "url": "http://supervisord.org/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://www.sonarqube.org/favicon-152.png",
-                "name": "SonarQube",
-                "desc": "代码质量和安全性检查, 支持所有开发人员编写更干净，更安全的代码。",
-                "url": "https://www.sonarqube.org/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://paste.ubuntu.com/favicon.ico",
-                "name": "Pastebin",
-                "desc": "在线共享代码，将代码以URL形式发送以便查看",
-                "url": "https://paste.ubuntu.com/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://avatars2.githubusercontent.com/u/30217756?s=200&v=4",
-                "name": "UPX",
-                "desc": "UPX-可执行文件的终极打包器",
-                "url": "https://github.com/upx/upx",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://avatars2.githubusercontent.com/u/30217756?s=200&v=4",
-                "name": "askgit",
-                "desc": "使用SQL查询git存储库。生成报告，执行状态检查，分析代码库。",
-                "url": "https://github.com/augmentable-dev/askgit",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://avatars3.githubusercontent.com/u/1396951?s=200&v=4",
-                "name": "Sentry",
-                "desc": "Web应用程序，移动应用程序和游戏的实时崩溃报告。",
-                "url": "https://sentry.io/",
-                "urls": { "Github": "https://github.com/getsentry" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://randomuser.me/favicon.ico",
                 "name": "randomuser",
                 "desc": "随机用户生成器是用于生成占位符用户信息的免费API。 获取个人资料照片，姓名等。 对于人们来说，就像Lorem Ipsum。",
                 "url": "https://randomuser.me/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://camo.githubusercontent.com/ebfdd465daa90626d0ab0d0fddc0e457c942a392/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f616e7572616768617a72612f696d6167652f75706c6f61642f76313539343930383234322f6c6f676f5f636373776d652e737667",
                 "name": "github-readme-stats",
                 "desc": "在你的 README 中 获取动态生成的 GitHub 统计信息",
-                "url": "https://github.com/anuraghazra/github-readme-stats/blob/master/readme_cn.md",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://i.imgur.com/zBEQq4w.png",
-                "name": "Hits",
-                "desc": "了解有多少人正在查看您的GitHub项目的简单方法",
-                "url": "http://hits.dwyl.io/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "GitHub 文件加速",
-                "desc": "GitHub 文件加速",
-                "url": "https://shrill-pond-3e81.hunsh.workers.dev/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
+                "url": "https://github.com/anuraghazra/github-readme-stats/blob/master/docs/readme_cn.md",
               },
               {
                 "name": "Shields IO",
                 "desc": "svg生成版本号图标",
                 "url": "https://shields.io/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://www.fundebug.com/favicon.ico",
-                "name": "FunDebug",
-                "desc": "支持前端JavaScript，后端Node.js以及微信小程序错误监控",
-                "url": "https://www.fundebug.com/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "http://www.asciiworld.com/favicon.ico",
-                "name": "Asciiworld",
-                "desc": "WA！原来那些年程序猿搞怪的注释都在这里",
-                "url": "http://www.asciiworld.com/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://jsfiddle.net/img/favicon.png",
                 "name": "jsfiddle",
                 "desc": "强大的前端代码在线演示的网站",
                 "url": "http://jsfiddle.net/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://user-images.githubusercontent.com/799578/50462941-8075fe80-09c3-11e9-89e7-af0cb7991406.png",
                 "name": "CODEIF",
                 "desc": "变量命名神器",
                 "url": "https://unbug.github.io/codelf/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "http://asciiflow.com/images/favicon.png",
                 "name": "ASCIIFlow Infinity",
                 "desc": "无限的ASCII图表，保存到谷歌驱动器，调整大小，自由绘制，并直接输出到文本/HTML。",
                 "url": "http://asciiflow.com/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://kinolien.github.io/gitzip/images/gitzip.png",
                 "name": "gitzip",
                 "desc": "它可以将GITHUB存储库的子文件夹/子目录作为zip并下载",
                 "url": "https://kinolien.github.io/gitzip/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "http://sc.ftqq.com/static/image/favlogo.png",
-                "name": "Server酱",
-                "desc": "「Server酱」，英文名「ServerChan」，是一款「程序员」和「服务器」之间的通信软件。",
-                "url": "http://sc.ftqq.com",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "mkcert",
-                "desc": "一个简单的零配置工具，可以使用您喜欢的任何名称制作本地可信赖的SSL开发证书",
-                "url": "https://github.com/FiloSottile/mkcert",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://carbon.now.sh/favicon.ico",
                 "name": "carbon",
                 "desc": "创建和分享源代码的精美图像",
                 "url": "https://carbon.now.sh",
                 "urls": { "Github": "https://github.com/dawnlabs/carbon" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "setup-ipsec-vpn",
-                "desc": "用于构建您自己的IPsec VPN服务器的脚本，在Ubuntu，Debian和CentOS上使用IPsec / L2TP和Cisco IPsec",
-                "url": "https://github.com/hwdsl2/setup-ipsec-vpn",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://www.digitalocean.com/favicon.ico",
-                "name": "nginxconfig",
-                "desc": "nginx配置生成器",
-                "url": "https://www.digitalocean.com/community/tools/nginx#?",
-                "urls": {
-                  "Github": "https://github.com/digitalocean/nginxconfig.io"
-                },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "GraphQL Explorer",
-                "desc": "GraphQL资源管理器利用真实的、实时的生产数据",
-                "url": "https://developer.github.com/v4/explorer/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "patorjk",
                 "desc": "将文本转换成ASCII",
-                "url": "http://patorjk.com/software/taag/#p=display&f=Graffiti&t=xiejiahe",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
+                "url": "http://patorjk.com/software/taag/#p=display&f=Graffiti&t=cola",
               },
               {
-                "icon": "https://avatars0.githubusercontent.com/u/62133242?s=200&v=4",
-                "name": "lens",
-                "desc": "Lens控制Kubernetes集群所需的唯一IDE。它是适用于MacOS，Windows和Linux操作系统的独立应用程序。它是开源的，免费的。",
-                "url": "https://k8slens.dev/",
-                "urls": { "Github": "https://github.com/lensapp/lens" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "name": "public-apis",
-                "desc": "一些可用在 Web 或软件开发的开放 API 接口",
-                "url": "https://github.com/public-apis/public-apis",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://astexplorer.net/favicon.png",
-                "name": "astexplorer",
-                "desc": "一个Web工具，用于探索由各种解析器生成的AST",
-                "url": "https://astexplorer.net/",
-                "urls": { "Github": "https://github.com/fkling/astexplorer" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "http://www.u.tools/assets/img/brand/favicon.png",
                 "name": "uTools",
                 "desc": "uTools是一个极简、插件化、跨平台的现代桌面软件。通过自由选配丰富的插件，打造你得心应手的工具集合",
                 "url": "http://www.u.tools/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://ihateregex.io/favicon.ico",
                 "name": "ihateregex",
                 "desc": "正则表达式备忘录",
                 "url": "https://ihateregex.io/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
-                "icon": "https://avatars3.githubusercontent.com/u/22552083?s=200&v=4",
-                "name": "Oh My ZSH",
-                "desc": "是一个开放源代码，社区驱动的框架，用于管理zsh配置。",
-                "url": "https://ohmyz.sh/",
-                "urls": { "Github": "https://github.com/ohmyzsh" },
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://tampermonkey.freetls.fastly.net/images/icon144.png",
                 "name": "Tampermonkey",
                 "desc": "一款免费的浏览器扩展和最为流行的用户脚本管理器",
                 "url": "https://www.tampermonkey.net/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
               {
                 "name": "Greasy Fork",
                 "desc": "提供用户脚本的网站",
                 "url": "https://greasyfork.org/zh-CN",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
               },
-              {
-                "icon": "https://img.kuaidaili.com/img/favicon.ico?v=3",
-                "name": "免费代理",
-                "desc": "免费HTTP代理IP",
-                "url": "https://www.kuaidaili.com/free/inha/1/",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              },
-              {
-                "icon": "https://avatars2.githubusercontent.com/u/1342004?s=200&v=4",
-                "name": "Google Workspace",
-                "desc": "Google Workspace状态信息中心",
-                "url": "https://www.google.com/appsstatus#hl=en&v=status",
-                "createdAt": "2021-01-23T01:32:06.423Z",
-                "urls": {},
-                "rate": 5,
-                "top": false
-              }
             ],
-            "collapsed": false,
-            "createdAt": "2021-01-23T01:32:06.423Z"
           },
           {
             "title": "接口/开发文档管理",

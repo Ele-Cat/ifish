ea93d12b8a68bd0c39547dd370782ddaf6c7cda8
lijiamao <951572198@qq.com>
2023-11-09 09:08:23 +0800

    🚑移除nav缓存

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { useAppStore } from "./modules/app";
-import { useNavStore } from "./modules/nav";
 import { useContextMenuStore } from "./modules/contextMenu";
 import { useMusicStore } from "./modules/music";
 import { useSystemStore } from "./modules/system";
@@ -7,7 +6,6 @@ import { useSystemStore } from "./modules/system";
 // 将store统一到一起再分发
 const useStore = () => ({
   useAppStore: useAppStore(),
-  useNavStore: useNavStore(),
   useContextMenuStore: useContextMenuStore(),
   useMusicStore: useMusicStore(),
   useSystemStore: useSystemStore(),

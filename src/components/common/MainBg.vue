fcc20ca5c1a72d4885e01b6c04897c1c05f464d5
ele-cat <951572198@qq.com>
2023-11-12 22:43:32 +0800

    ✨完善纯色壁纸

--- src/components/common/MainBg.vue
+++ src/components/common/MainBg.vue
@@ -1,10 +1,40 @@
 <template>
   <div class="main-bg">
-    <img v-if="useSystemStore.settings.wallpaper.type === 'basic'" class="cover" :src="useSystemStore.settings.wallpaper.url" alt="">
-    <img v-if="useSystemStore.settings.wallpaper.type === 'basic'" class="cover animate__fadeOut" :src="useSystemStore.settings.wallpaper.url" alt="">
-    <video v-if="useSystemStore.settings.wallpaper.type === 'dynamics'" class="video" :src="useSystemStore.settings.wallpaper.url" playsinline autoplay loop muted></video>
-    <div v-if="useSystemStore.settings.wallpaper.type === 'pure'" class="pure" :style="{backgroundImage: `linear-gradient(${useSystemStore.settings.wallpaper.angle}deg, ${useSystemStore.settings.wallpaper.pure[0]} 15%, ${useSystemStore.settings.wallpaper.pure[1]} 150%)`}"></div>
-    <div class="mask" :style="{backgroundColor: `rgba(0, 0, 0, ${useSystemStore.settings.wallpaper.mask / 100})`,backdropFilter: `blur(${useSystemStore.settings.wallpaper.blur}px)`}"></div>
+    <img
+      v-if="useSystemStore.settings.wallpaper.type === 'basic'"
+      class="cover"
+      :src="useSystemStore.settings.wallpaper.url"
+      alt=""
+    />
+    <img
+      v-if="useSystemStore.settings.wallpaper.type === 'basic'"
+      class="cover animate__fadeOut"
+      :src="useSystemStore.settings.wallpaper.url"
+      alt=""
+    />
+    <video
+      v-if="useSystemStore.settings.wallpaper.type === 'dynamics'"
+      class="video"
+      :src="useSystemStore.settings.wallpaper.url"
+      playsinline
+      autoplay
+      loop
+      muted
+    ></video>
+    <div
+      v-if="useSystemStore.settings.wallpaper.type === 'pure'"
+      class="pure"
+      :style="{
+        backgroundImage: `linear-gradient(${useSystemStore.settings.wallpaper.angle}deg, ${useSystemStore.settings.wallpaper.pure[0]} 15%, ${useSystemStore.settings.wallpaper.pure[1]} 150%)`,
+      }"
+    ></div>
+    <div
+      class="mask"
+      :style="{
+        backgroundColor: `rgba(0, 0, 0, ${useSystemStore.settings.wallpaper.mask / 100})`,
+        backdropFilter: `blur(${useSystemStore.settings.wallpaper.blur}px)`,
+      }"
+    ></div>
   </div>
 </template>
 
@@ -15,15 +45,20 @@ const { useSystemStore } = useStore();
 
 const type = ref(useSystemStore.settings.wallpaper.type);
 const url = ref(useSystemStore.settings.wallpaper.url);
-watch(() => useSystemStore.settings.wallpaper, newVal => {
-  type.value = newVal.type;
-  url.value = newVal.url;
-}, {
-  immediate: true,
-  deep: true,
-})
+watch(
+  () => useSystemStore.settings.wallpaper,
+  (newVal) => {
+    type.value = newVal.type;
+    url.value = newVal.url;
+  },
+  {
+    immediate: true,
+    deep: true,
+  }
+);
 
-const coverUrl = "https://imgse.com/content/images/system/home_cover_1601010270144_8921bc.jpg";
+const coverUrl =
+  "https://imgse.com/content/images/system/home_cover_1601010270144_8921bc.jpg";
 // const videoUrl = "https://img2.picknewtab.com/wallpapers_video/648fb71175652244eaa103a0/435582351.mp4"
 // const videoUrl = "./images/bg/disney.mp4"
 </script>
@@ -37,10 +72,9 @@ const coverUrl = "https://imgse.com/content/images/system/home_cover_16010102701
     width: 100vw;
     height: 100vh;
     object-fit: cover;
-    transition: all .3s;
-    background: linear-gradient(135deg,#75f0db,#75dbf0,#8a75f0,#f075c7);
+    transition: all 0.3s;
+    background: linear-gradient(135deg, #75f0db, #75dbf0, #8a75f0, #f075c7);
     background-size: 200% 200%;
-    animation: gradient-move 5s ease infinite alternate;
   }
   .animate__fadeOut {
     width: 200vw;
@@ -65,6 +99,8 @@ const coverUrl = "https://imgse.com/content/images/system/home_cover_16010102701
     top: 0px;
     width: 100vw;
     height: 100vh;
+    background-size: 200% 200%;
+    animation: gradient-move 5s ease infinite alternate;
   }
   .mask {
     position: fixed;

eeda7a1fb338d74a9d670b4fed754b28b4be007e
ele-cat <951572198@qq.com>
2023-11-21 23:01:51 +0800

    ✨完善伪装升级

--- src/components/common/Apps/FakeUpdate/Index.vue
+++ src/components/common/Apps/FakeUpdate/Index.vue
@@ -1,32 +1,45 @@
 <template>
-  <img :src="app.icon" class="app-img" alt="" @click.stop="dialogVisible = true">
+  <img :src="app.icon" class="app-img" alt="" @click.stop="dialogVisible = true" />
 
-  <IDialog title="伪装升级" :width="640" :visible="dialogVisible" @cancel="dialogVisible = false">
+  <IDialog
+    title="伪装升级"
+    :width="640"
+    :visible="dialogVisible"
+    @cancel="dialogVisible = false"
+  >
     <div class="fake-update">
       <p class="title">请选择一个主题</p>
       <div class="fakes">
         <!-- https://fakeupdate.net/ -->
-        <div class="fake-item" v-for="(fake, index) in fakes" :key="index" @click="handleUseFake(fake)">
-          <img :src="fake.cover" alt="">
+        <div
+          class="fake-item"
+          :title="fake.label"
+          v-for="(fake, index) in fakes"
+          :key="index"
+          @click="handleUseFake(fake)"
+        >
+          <img :src="fake.cover" alt="" />
         </div>
       </div>
-      <p class="tips">本功能会让你的电脑进入一个假更新的画面，让别人以为你的电脑正在升级，这时候你就可以休息一下，优雅地喝一杯咖啡。不要有内疚感，适当的休息可以让你的大脑重新充满活力，能面对更多挑战，更高效地完成工作。</p>
+      <p class="tips">
+        本功能会让你的电脑进入一个假更新的画面，让别人以为你的电脑正在升级，这时候你就可以休息一下，优雅地喝一杯咖啡。不要有内疚感，适当的休息可以让你的大脑重新充满活力，能面对更多挑战，更高效地完成工作。
+      </p>
     </div>
+    <component ref="fakeRef" v-if="activeFake.label" :is="activeFake.component" />
   </IDialog>
-
-  <img ref="fakeRef" v-show="activeFake.cover" :src="activeFake.cover" alt="">
 </template>
 
 <script setup>
-import { reactive, ref } from "vue"
+import { nextTick, onMounted, reactive, ref } from "vue";
 import { useFullscreen } from "@vueuse/core";
+import WindowsXp from "./WindowsXp.vue";
 
 const props = defineProps({
   app: {
     type: Object,
     default: () => {},
-  }
-})
+  },
+});
 
 const dialogVisible = ref(false);
 const activeFake = ref({});
@@ -34,6 +47,7 @@ const fakes = reactive([
   {
     label: "Windows XP",
     cover: "https://i.postimg.cc/PrG8Pb9Q/2.png",
+    component: WindowsXp,
   },
   {
     label: "Windows Vista",
@@ -54,17 +68,25 @@ const fakes = reactive([
   {
     label: "MacOS",
     cover: "https://fakeupdate.net/assets/img/6.png",
-  }
-])
+  },
+]);
 
 const fakeRef = ref(null);
-const { isFullscreen, toggle, enter } = useFullscreen(fakeRef);
+const { isFullscreen, toggle, enter, exit } = useFullscreen(fakeRef);
 
 const handleUseFake = (fake) => {
-  activeFake.value = fake;
-  console.log('fake: ', fake);
-  enter();
-}
+  // activeFake.value = fake;
+  // nextTick(() => {
+  //   enter();
+  // });
+  // document.addEventListener("keydown", function (event) {
+  //   console.log("event: ", event);
+  //   if (event.key === "Escape") {
+  //     exit();
+  //     activeFake.value = {};
+  //   }
+  // });
+};
 </script>
 
 <style lang="less" scoped>
@@ -76,14 +98,15 @@ const handleUseFake = (fake) => {
   .fakes {
     display: flex;
     flex-wrap: wrap;
-    // justify-content: space-between;
+    justify-content: space-between;
     margin-top: 20px;
     .fake-item {
-      width: 33%;
-      margin-right: 0.3%;
+      width: 32%;
+      margin: 0 0 10px 0;
       cursor: pointer;
       img {
         width: 100%;
+        height: 120px;
       }
       &:hover {
         box-shadow: 0 0 6px red;
---
+++ src/components/common/Apps/FakeUpdate/WindowsXp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fake-box">
+    WindowsXp
+    <!-- http://fediafedia.com/fakeupdate.net/assets/img/N9nbgDY.jpg -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+.fake-box {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #fff;
+  z-index: 999999;
+  cursor: url(http://fediafedia.com/fakeupdate.net/assets/img/gQyFbtU.png), auto;
+}
+</style>

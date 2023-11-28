6ae2e197d1777e78488e733a1b059f857055570f
lijiamao <951572198@qq.com>
2023-11-28 15:14:13 +0800

    🚑恢复新闻资讯

--- src/store/modules/news.js
+++ src/store/modules/news.js
@@ -4,14 +4,7 @@ import { newsList } from "@/mock/news";
 export const useNewsStore = defineStore("ifishNews", {
   state: () => {
     return {
-      lists: newsList.map(item => {
-        return {
-          label: item,
-          value: item,
-          visible: true,
-        }
-      }),
-      activeNews: 0,
+      lists: newsList,
     };
   },
   actions: {
